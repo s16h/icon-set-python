8d4cb771339a88a488edf3cc0d83a2954b24e058
Shahriar Tajbakhsh <sh.tajbakhsh@gmail.com>
2017-03-02 19:05:24 +0000

    Ability to create an Apple Icon Set from a PNG file

--- icon_set/icon_set.py
+++ icon_set/icon_set.py
@@ -1,1 +1,44 @@
-# -*- coding: utf-8 -*-
+from io import BytesIO
+
+from PIL import Image
+
+from .header import Header
+from .icon_data import IconData
+from .icon_type import icon_types
+
+
+class IconSet():
+    def __init__(self):
+        self.icon_data = []
+
+    def add_icon_data(self, icon_data):
+        self.icon_data.append(icon_data)
+
+    def as_bytearray(self):
+        header_length = 4
+        file_length = (
+            header_length + sum(len(i.as_bytearray()) for i in self.icon_data)
+        )
+        header = Header(file_length)
+
+        payload = header.as_bytearray()
+        for icon_data in self.icon_data:
+            payload += icon_data.as_bytearray()
+
+        return payload
+
+    @classmethod
+    def from_png(cls, png_file_path):
+        instance = cls()
+        base_image = Image.open(png_file_path)
+        for icon_type in icon_types:
+            to_size = [icon_type.size, icon_type.size]
+
+            new_image = base_image.resize(to_size, Image.ANTIALIAS)
+            new_image_bytes = BytesIO()
+            new_image.save(new_image_bytes, format='PNG')
+
+            icon_data = IconData(icon_type, icon=new_image_bytes.getvalue())
+            instance.add_icon_data(icon_data)
+
+        return instance
--- icon_set/icon_type.py
+++ icon_set/icon_type.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from .exceptions import InvalidIconTypeError
 
 
@@ -21,3 +23,17 @@ class IconType(object):
         +-----------------------------+
         """
         return bytearray(self.os_type, encoding='ascii')
+
+
+icon_types = [
+    IconType(os_type='icp4', size=16, description='16x16 icon in JPEG 2000 or PNG format'),  # noqa
+    IconType(os_type='icp5', size=32, description='32x32 icon in JPEG 2000 or PNG format'),  # noqa
+    IconType(os_type='ic07', size=128, description='128x128 icon in JPEG 2000 or PNG format'),  # noqa
+    IconType(os_type='ic08', size=256, description='256×256 icon in JPEG 2000 or PNG format'),  # noqa
+    IconType(os_type='ic09', size=512, description='512×512 icon in JPEG 2000 or PNG format'),  # noqa
+    IconType(os_type='ic10', size=1024, description='1024×1024 in 10.7 (or 512x512@2x "retina" in 10.8) icon in JPEG 2000 or PNG format'),  # noqa
+    IconType(os_type='ic11', size=32, description='16x16@2x "retina" icon in JPEG 2000 or PNG format'),  # noqa
+    IconType(os_type='ic12', size=64, description='32x32@2x "retina" icon in JPEG 2000 or PNG format'),  # noqa
+    IconType(os_type='ic13', size=256, description='128x128@2x "retina" icon in JPEG 2000 or PNG format'),  # noqa
+    IconType(os_type='ic14', size=512, description='256x256@2x "retina" icon in JPEG 2000 or PNG format')  # noqa
+]
